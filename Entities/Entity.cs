8919a2bc0da29e61e95109d9a21ae82bd19f48cd
Alexander Friberg <alexanderfriberg1999@gmail.com>
2026-04-03 11:11:03 +0200

    refactor(Entity): slight refactoring

--- Entities/Entity.cs
+++ Entities/Entity.cs
@@ -1,16 +1,39 @@
 namespace RogueConsole.Entities;
 
-abstract class Entity
+using RogueConsole.Core;
+using RogueConsole.World;
+
+public abstract class Entity
 {
-    public int X { get; set; }
-    public int Y { get; set; }
-    public char Glyph { get; set; }
+    public EntityType Type { get; init; }
     public int Health { get; set; }
     public int MaxHealth { get; set; }
     public bool IsDead => Health <= 0;
+    protected TileMap _map;
+
+    protected Entity(TileMap map, int health, int maxhealth, EntityType type)
+    {
+        Health = health;
+        MaxHealth = maxhealth;
+        _map = map;
+        Type = type;
+    }
 
     public virtual void Update()
     {
         // override per entity type
     }
+
+    public virtual void CheckState(object? sender, GamePhase phase)
+    {
+
+    }
+
+
+}
+
+public enum EntityType
+{
+    Player,
+    Enemy
 }
